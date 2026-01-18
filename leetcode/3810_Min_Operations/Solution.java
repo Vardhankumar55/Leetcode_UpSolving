9bac2c026b035ebe88dfe3b3cc0b97f81102d625
Vardhan <vardhankumar68@gmail.com>
2026-01-18 16:53:43 +0530

    Remove unnecessary comments from Solution.java

--- leetcode/3810_Min_Operations/Solution.java
+++ leetcode/3810_Min_Operations/Solution.java
@@ -1,5 +1,3 @@
-// Minimal valid Java source to remove syntax errors.
-// Non-public class so filename need not match the class name.
 class Solution {
     public int minOperations(int[] nums, int[] target){
         int n = nums.length;
