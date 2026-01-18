1ab15849f69ab580d268811f2cf6543bda7c7785
Vardhan <vardhankumar68@gmail.com>
2026-01-18 16:54:35 +0530

    Refactor minOperations to use a HashSet
    Use a set to count unique elements for operations.

--- leetcode/3810_Min_Operations/Solution.java
+++ leetcode/3810_Min_Operations/Solution.java
@@ -1,7 +1,7 @@
 class Solution {
     public int minOperations(int[] nums, int[] target){
         int n = nums.length;
-        Set<Integer> set=new HashSet<>();
+        Set<Integer> set=new HashSet<>();//used bcoz we count the similar elemts as one operation only 
         for(int i=0;i<n;i++)
 {
             if(nums[i]!=target[i]){
